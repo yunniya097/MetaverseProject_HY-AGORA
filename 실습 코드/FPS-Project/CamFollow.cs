8805dd4064ad04bc26c3d14121b9e97d65baf19b
Seyun Ahn <81553569+yunniya097@users.noreply.github.com>
2022-01-27 03:45:22 +0900

    Create CamFollow.cs

---
+++ 실습 코드/FPS-Project/CamFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    // transform component 변수 설정
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 카메라의 위치를 target의 위치에 일치
+        transform.position = target.position;
+    }
+}
