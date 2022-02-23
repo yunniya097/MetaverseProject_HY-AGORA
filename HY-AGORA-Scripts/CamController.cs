a047c7432a772a8a09312f338598615f2e5e1c02
Seyun Ahn <81553569+yunniya097@users.noreply.github.com>
2022-02-24 04:47:43 +0900

    finished

--- HY-AGORA-Scripts/CamController.cs
+++ HY-AGORA-Scripts/CamController.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class CamController : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject player; // 바라볼 플레이어 오브젝트
+    public GameObject chair; // 클릭 인식 할 의자 오브젝트
+    
+    
+    public float xmove = 0;  // X축 누적 이동량
+    public float ymove = 0;  // Y축 누적 이동량
+    public float distance = 3;
+
+    public float SmoothTime = 0.2f;
+    private Vector3 velocity = Vector3.zero;
+
+    private int toggleView = 3; // 1=1인칭, 3=3인칭
+
+    void Update()
     {
-        offset = transform.position - player.transform.position;
-    }
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        transform.position = player.transform.position + offset;
+        xmove += Input.GetAxis("Mouse X"); // 마우스의 좌우 이동량을 xmove에 누적
+        ymove -= Input.GetAxis("Mouse Y"); // 마우스의 상하 이동량을 ymove에 누적
+        transform.rotation = Quaternion.Euler(ymove, xmove, 0); // 이동량에 따라 카메라의 바라보는 방향을 조정
+        
+
+        // 의자 오브젝트 클릭 시 토글뷰를 1로 변환
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform.gameObject.tag == "Chair")
+                {
+                    Debug.Log("HIT Chair");
+                    toggleView = 4 - toggleView;
+                }
+            }
+        }
+        
+        // 토글 뷰가 1이면 1인칭 시점
+        if (toggleView == 1)
+        {
+            Vector3 reverseDistance = new Vector3(0.0f, 0.4f, 0.2f); // 카메라가 바라보는 앞방향은 Z 축입니다. 이동량에 따른 Z 축방향의 벡터를 구합니다.
+            transform.position = player.transform.position + transform.rotation * reverseDistance; // 플레이어의 위치에서 카메라가 바라보는 방향에 벡터값을 적용한 상대 좌표를 차감합니다.
+        }
+        // 토글 뷰가 3이면 3인칭 시점
+        else if (toggleView == 3)
+        {
+            Vector3 reverseDistance = new Vector3(0.0f, 0.0f, distance); // 카메라가 바라보는 앞방향은 Z 축입니다. 이동량에 따른 Z 축방향의 벡터를 구합니다.
+            transform.position = player.transform.position - transform.rotation * reverseDistance; // 플레이어의 위치에서 카메라가 바라보는 방향에 벡터값을 적용한 상대 좌표를 차감합니다.
+        }
     }
 }
