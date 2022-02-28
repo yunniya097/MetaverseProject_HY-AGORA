2b456faa0209e3ae947c915894a161a8cda7e748
Seyun Ahn <81553569+yunniya097@users.noreply.github.com>
2022-03-01 03:47:02 +0900

    Update TimerCtrlBtn.cs

--- HY-AGORA-Scripts/TimerCtrlBtn.cs
+++ HY-AGORA-Scripts/TimerCtrlBtn.cs
@@ -6,28 +6,26 @@ public class TimerCtrlBtn : MonoBehaviour
 {
     public bool ButtonActive; // 지금 숫자 세고 있는지
     public GameObject TimerManager;
+    public GameObject GameManager;
+    public GameObject GameStartButton;
+
     Timer tm;
     GameObjClick objt;
-    ScoreManager sm;
     
     void Start()
     {
         ButtonActive = false;
         tm = TimerManager.GetComponent<Timer>();
-        objt = GetComponent<GameObjClick>();
-        sm = GetComponent<ScoreManager>();
-        
-    }
-
-    void Update()
-    {
-        
+        objt = GameManager.GetComponent<GameObjClick>();
+        GameStartButton.SetActive(false);
     }
 
     public void ClickButton()
     {
         if(!ButtonActive)
         {
+            /*수정된거*/
+            GameStartButton.SetActive(false);
             // 시작해야 되는 부분
             tm.SetTimerOn();
             objt.SetGameOn();
@@ -39,9 +37,17 @@ public class TimerCtrlBtn : MonoBehaviour
             tm.SetTimerStop();
             objt.SetGameOff();
             ButtonActive = false;
-            sm.GameOver();
-            
         }
+    }
+
+    public void ButtonView()
+    {
+        GameStartButton.SetActive(true);
+    }
+
+    public void ButtonUnView()
+    {
+        GameStartButton.SetActive(false);
     }
 
     
