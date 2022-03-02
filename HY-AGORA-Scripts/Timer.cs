e6e6c6db5bdcfdeb29baa99ccd01189e75312daa
Seyun Ahn <81553569+yunniya097@users.noreply.github.com>
2022-03-02 22:01:40 +0900

    Update Timer.cs

--- HY-AGORA-Scripts/Timer.cs
+++ HY-AGORA-Scripts/Timer.cs
@@ -5,23 +5,60 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    public GameObject timer;
     public Text timerTxt;
     public float time = 60f;
     private float selectCountdown;
+    bool TimerOn; // timer가 돌아가고 있는지
+    GameObjClick objt;
 
+    void Awake()
+    {
+        timer.SetActive(false);
+    }
+    // Start is called before the first frame update
     void Start()
     {
+        TimerOn = false;
         selectCountdown = time;
+        objt = GetComponent<GameObjClick>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if(Mathf.Floor(selectCountdown) <= 0) {
-            // Count 0일때 동작할 함수 삽입
-        } else {
-            selectCountdown -= Time.deltaTime;
-            timerTxt.text = Mathf.Floor(selectCountdown).ToString();
+        if(TimerOn) 
+        {
+            if(Mathf.Floor(selectCountdown) > 0)
+            {
+                selectCountdown -= Time.deltaTime;
+                timerTxt.text = Mathf.Floor(selectCountdown).ToString();
+            }
+            else
+            {
+                SetTimerStop();
+            }
         }
+
+    }
+
+    public void TimerView()
+    {
+        timer.SetActive(true);
+    }
+    public void TimerUnView()
+    {
+        timer.SetActive(false);
+    }
+
+    public void SetTimerOn()
+    {
+        TimerOn = true;
+    }
+
+    public void SetTimerStop()
+    {
+        TimerOn = false;
     }
 
 }
